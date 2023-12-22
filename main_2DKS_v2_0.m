415707f289cd802270c1c1dce8e0c7523b1becbd
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2023-12-22 15:43:16 -0500

    Add files via upload

--- main_2DKS_v2_0.m
+++ main_2DKS_v2_0.m
@@ -7,7 +7,7 @@ function main_2DKS_v2_0
     IC = 'sin'; % initial condition
 
     %%% choose parameter ranges %%%
-    timewindow = linspace(0,120,7); % for time-stepping analysis
+    timewindow = linspace(0,60,7); % for time-stepping analysis
     timewindow(1) = 10^(-3);
     L_scale = linspace(0,20,11)/10; % for dynamical behavior analysis
     L_scale(1) = 10^(-6);
@@ -17,11 +17,11 @@ function main_2DKS_v2_0
 
     %%% choose default parameters %%%
     L_s1 = 1; % length-scale parameter in dim 1
-    L_s2 = L_scale(2); % length-scale parameter in dim 2
+    L_s2 = L_scale(10); % length-scale parameter in dim 2
     dt = timestep(5); % length of time-step
-    T = timewindow(7); % time window
+    T = timewindow(2); % time window
     N = gridsize(3); % number of grid points 
-    save_each = 200; % number of iterations between saved timepoints
+    save_each = 100; % number of iterations between saved timepoints
 
     switch run % set which test to run
         case 'L'
@@ -40,7 +40,7 @@ function main_2DKS_v2_0
         close all;
     
         %%% set variable parameters %%%
-        L_s1 = L_s2;
+        % L_s1 = L_s2;
         switch run 
             case 'L'
                 L_s2 = L_scale(k); % length-scale parameter in dim 2 
@@ -53,25 +53,28 @@ function main_2DKS_v2_0
         end
         
         %%% solve PDE problem in time %%%
-        %
+        %{
         tic
         [ v_n , u_n ] = DirectSolve_2DKS_v1_0(IC,method,N,L_s1,L_s2,dt,T,save_each);
         time = toc;
-        %
+        %}
         
         %%% save/inspect solution %%%
         %
-        save_2DKSsolution('time_evolution', u_n, v_n, time, method, dt, T, N, L_s1, L_s2); % save solution
+        % save_2DKSsolution('time_evolution', u_n, v_n, time, method, dt, T, N, L_s1, L_s2); % save solution
         %
-        plot2DKS(v_n , u_n, 'initial', method, N, dt, T, L_s1, L_s2); % save/inspect initial state
-        plot2DKS(v_n , u_n, 'terminal', method, N, dt, T, L_s1, L_s2); % save/inspect terminal state
-        plot2DKS(v_n , u_n, 'gif_contour', method, N, dt, T, L_s1, L_s2); % save/inspect contour time evolution
-        plot2DKS(v_n , u_n, 'gif', method, N, dt, T, L_s1, L_s2); % save/inspect surface time evolution
+        [u_n, v_n, ~] = load_2DKSsolution('time_evolution', method, dt, T, N, L_s1, L_s2); % load solution
+        %
+        % plot2DKS(v_n , u_n, 'initial', method, N, dt, T, L_s1, L_s2); % save/inspect initial state
+        % plot2DKS(v_n , u_n, 'terminal', method, N, dt, T, L_s1, L_s2); % save/inspect terminal state
+        plot2DKS(v_n , u_n, 'largegif_contour', method, N, dt, T, L_s1, L_s2); % save/inspect contour time evolution
+        plot2DKS(v_n , u_n, 'largegif', method, N, dt, T, L_s1, L_s2); % save/inspect surface time evolution
         %
     
     end
     %
 
+    %{
     switch run 
         case 'N' % spatial convergence: error analysis and computational time
             [error_2,error_inf,comptime] = spatialconvergence_2DKS(gridsize, method, dt, T, L_s1, L_s2);
@@ -80,5 +83,6 @@ function main_2DKS_v2_0
             [error_2,error_inf,comptime] = temporalconvergence_2DKS(timestep, method, N, T, L_s1, L_s2);
             save_measures('temporal', error_2, error_inf, comptime, method, N, 0, T, L_s1, L_s2);
     end
+    %}
 
 end
