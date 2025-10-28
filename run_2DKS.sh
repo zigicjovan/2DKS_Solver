3529154436ccf18d69fa7dd0f0d8624a084a6146
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2025-10-28 00:37:10 -0400

    Add files via upload

--- run_2DKS.sh
+++ run_2DKS.sh
@@ -2,18 +2,12 @@
 #SBATCH --account=def-bprotas                # RAC account for Bartek, to be used for priority jobs
 #SBATCH --mail-user=zigicj@mcmaster.ca       # email address
 #SBATCH --mail-type=ALL                      # Send email regarding all events with job
-#SBATCH --nodes=1		             # number of nodes (32 cpus per node)
-#SBATCH --ntasks-per-node=32	             # number of MPI processes
-#SBATCH --mem=0M		             # total memory required; default unit is megabytes
-#SBATCH --time=01-00:00		             # time (DD-HH:MM) or (HH:MM:SS)
-#SBATCH --job-name=K3ell112120               # keep track of jobs
+#SBATCH --ntasks=1                           # Serial code - keep at 1
+#SBATCH --cpus-per-task=8                    
+#SBATCH --mem=0M		                     # total memory required; default unit is megabytes
+#SBATCH --time=03-00:00		                 # time (DD-HH:MM) or (HH:MM:SS)
+#SBATCH --job-name=K0ell112120               # keep track of jobs
 
 module load matlab/2024b.1
 
-# Run your MATLAB script (without GUI)
-nohup matlab -nodisplay -nodesktop -nosplash -r "main_2DKS(5e-05,48,3,3,1,1.12,1.20,0.02,-0.5,0.2,8); exit" > outK3ell112120.log 2>&1 < /dev/null &
-seff $SLURM_JOBID		# print short summary of cpu and memory efficiency (for more details: sacct -j <jobid>)
-
-
-
-
+matlab -nodisplay -nodesktop -nosplash -r "main_2DKS(1e-03,24,0,0,1,1.12,1.20,0.02,1.0,2.0,11); exit" > K0ell112120.log 2>&1 < /dev/null
