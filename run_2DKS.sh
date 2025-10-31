9348dcdb61fb3c55334d3987c553a48adbbb9eef
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2025-10-30 22:13:21 -0400

    Add files via upload

--- run_2DKS.sh
+++ run_2DKS.sh
@@ -4,10 +4,10 @@
 #SBATCH --mail-type=ALL                      # Send email regarding all events with job
 #SBATCH --ntasks=1                           # Serial code - keep at 1
 #SBATCH --cpus-per-task=8                    
-#SBATCH --mem=0M		                     # total memory required; default unit is megabytes
-#SBATCH --time=03-00:00		                 # time (DD-HH:MM) or (HH:MM:SS)
-#SBATCH --job-name=K0ell112120               # keep track of jobs
+#SBATCH --mem=32G		             # total memory required; default unit is megabytes
+#SBATCH --time=05-00:00		             # time (DD-HH:MM) or (HH:MM:SS)
+#SBATCH --job-name=K5ell106-26               # keep track of jobs
 
 module load matlab/2024b.1
 
-matlab -nodisplay -nodesktop -nosplash -r "main_2DKS(1e-03,24,0,0,1,1.12,1.20,0.02,1.0,2.0,11); exit" > K0ell112120.log 2>&1 < /dev/null
+matlab -nodisplay -nodesktop -nosplash -r "main_2DKS(5e-08,192,5,5,1,1.06,1.06,0.02,-2.6,-2.6,1,'optimize','IC',1e-6,0.0); exit" > ./output/K5ell106-26.log 2>&1 < /dev/null
