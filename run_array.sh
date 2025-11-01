a3d560546aa33f76b70f4e04e03c7418b449cb6a
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2025-10-31 23:11:10 -0400

    Update run_array.sh

--- run_array.sh
+++ run_array.sh
@@ -4,22 +4,12 @@ if [[ "$1" == "--dry-run" ]]; then DRY_RUN=1; fi
 mkdir -p slurm_logs output runscripts
 echo "(run_array.sh) dry-run=$DRY_RUN"
 
-echo "Group memory=32G: tasks=11, param_file=runscripts/task_params_32G.txt"
+echo "Group memory=32G: tasks=84, param_file=runscripts/task_params_32G.txt"
 if [[ $DRY_RUN -eq 0 ]]; then
   sbatch --account=def-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \
          --ntasks=1 --cpus-per-task=8 --time=07-00:00 --mem=32G \
-         --array=0-10 --output=slurm_logs/slurm-%A_%a.out --error=slurm_logs/slurm-%A_%a.err \
+         --array=0-83 --output=slurm_logs/slurm-%A_%a.out --error=slurm_logs/slurm-%A_%a.err \
          --export=ALL,PARAM_FILE=runscripts/task_params_32G.txt ./run_task_array.sh
 else
-  echo "Dry run: would sbatch --array=0-10 --mem=32G --cpus-per-task=8 --export=PARAM_FILE=runscripts/task_params_32G.txt ./run_task_array.sh"
-fi
-
-echo "Group memory=48G: tasks=11, param_file=runscripts/task_params_48G.txt"
-if [[ $DRY_RUN -eq 0 ]]; then
-  sbatch --account=def-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \
-         --ntasks=1 --cpus-per-task=8 --time=07-00:00 --mem=48G \
-         --array=0-10 --output=slurm_logs/slurm-%A_%a.out --error=slurm_logs/slurm-%A_%a.err \
-         --export=ALL,PARAM_FILE=runscripts/task_params_48G.txt ./run_task_array.sh
-else
-  echo "Dry run: would sbatch --array=0-10 --mem=48G --cpus-per-task=8 --export=PARAM_FILE=runscripts/task_params_48G.txt ./run_task_array.sh"
+  echo "Dry run: would sbatch --array=0-83 --mem=32G --cpus-per-task=8 --export=PARAM_FILE=runscripts/task_params_32G.txt ./run_task_array.sh"
 fi
