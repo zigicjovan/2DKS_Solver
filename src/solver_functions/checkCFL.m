901356573529e59ea001049956b0b7264eabfb97
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2026-07-22 10:50:50 -0400

    Delete src directory

--- src/solver_functions/checkCFL.m
+++
@@ -1,8 +0,0 @@
-function checkCFL(dx,dt,u)
-    C = 0.9;
-    u_Linf = max(max(abs(u)));
-    cflstep = C*dx/(max(1e-16,u_Linf));
-    if dt > cflstep
-        error(['Warning: CFL condition not upheld for solution coordinate = ' num2str(u_Linf) ' and dt = ' num2str(dt)]);
-    end
-end
