eebdad5968ae076c3c2da808b9c0b8e711856f58
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2026-08-30 16:17:00 -0400

    Change domain size sweep to logarithmic

--- param_driver.py
+++ param_driver.py
@@ -13,42 +13,42 @@ import time
 
 # User-editable parameter ranges
 K_start =           4.0
-K_end =             4.0
+K_end =             K_start
 K_step =            0.5
 num_modes_start =   1
 num_modes_end =     num_modes_start
-ell1_start =        1.35
-ell1_end =          1.35
-ell1_step =         0.15
+ell1_start =        0.00
+ell1_end =          0.00
+ell1_step =         0.5
 ell2_start =        ell1_start
 ell2_end =          ell1_end 
 ell2_step =         0.1
 K_range =           np.round(np.arange(K_start, K_end + K_step/2, K_step), 1)
-ell1_range =        np.round(np.arange(ell1_start, ell1_end + ell1_step/2, ell1_step), 2)
+ell1_range =        10**(np.round(np.arange(ell1_start + 0.01, ell1_end + ell1_step/2, ell1_step), 2))
 # ell1_range = [round(x, 2) for x in [np.sqrt(13),np.sqrt(16),np.sqrt(18),np.sqrt(20)]]
-ell2_range =        np.round(np.arange(ell2_start, ell2_end + ell2_step/2, ell2_step), 2)
+ell2_range =        10**(np.round(np.arange(ell2_start + 0.01, ell2_end + ell2_step/2, ell2_step), 2))
 # ell2_range = ell1_range
 
 # User-editable global settings
-SBATCH_TIME = "01-00:00"  # requested time limit (D-HH:MM) 
-RUN_ARRAY_NAME = f"b_{K_start}_{K_end}-{ell1_start:.2f}_{ell1_end:.2f}_{ell2_start:.2f}_{ell2_end:.2f}-branch.sh"
+SBATCH_TIME = "02-23:59"  # requested time limit (D-HH:MM) 
+RUN_ARRAY_NAME = f"long-{K_start}-{ell1_start:.2f}_{K_end}-{ell1_end:.2f}.sh"
 SEQUENTIAL_TASKS = False
 
 # Solver settings shared by all generated production runs
 IC = "s1"
-OPTIMIZE = 0
+OPTIMIZE = 1
 TOL = "1e-10"
-CONTINUATION = 0
-OPT_T = 1
-SAVE_STATES = 1
+CONTINUATION = 1
+OPT_T = -3.10
+SAVE_STATES = 100
 
 def generate_tasks():
     # Parameter choice formulas for 2DKS problem
     N_choice =  [ 64,   128,  192,  256,  384,  512,  640,  768,  896,  1024, 1536, 2048, 3072, 4096, 5120, 6144, 8192 ] 
-    dt_choice = [ 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 3e-7, 2e-7, 9e-8, 3e-8, 3e-8, 3e-8, 3e-8, 3e-8, 2e-9, 1e-9, 5e-10 ]
-    rk_choice = [ 2,    4,    6,    8,    12,   16,   20,   24,   28,   32,   48,   64,   96,   128,  160,  192,  256  ]
+    dt_choice = [ 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 3e-7, 2e-7, 9e-8, 3e-8, 2e-8, 1e-8, 7e-9, 5e-9, 3e-9, 2e-9, 1e-9 ]
+    rk_choice = [ 2,    4,    6,    8,    16,   64,   80,   96,   112,  128,  128,  128,  128,  256,  320,  384,  512  ]
     K_ref = 3.5
-    T_width = round(0.1, 2)
+    T_width = round(0.10, 2)
     T_step = round(0.05, 2)
 
     # generate parameter tuples
@@ -59,38 +59,39 @@ def generate_tasks():
         for ell1 in ell1_range:
             # for ell2 in ell2_range:
                 ell2 = ell1
-                elltemp = ell1
-                T_target = 0.5*ell1 + 2.0
+                # elltemp = ell1
+                T_target = np.log10( 10**(2.5 - K) * (ell1*ell2)**(0.85) ) 
                 # targettemp = 1.02*np.sqrt(1)
                 # T_target = 0.5*targettemp + 2.1
                 # T_range = np.round(np.array([(2*T_target / 3) - K ]), 2) # init
-                T_range = np.round(np.array([-1.30]), 2) # fixed
                 # T_range = np.round(np.array([(T_target) - K ]), 2) # Target
                 # T_range = np.round(np.array([(T_target - T_width) - K ]), 2) # Lower Bound LB
                 # T_range = np.round(np.array([(T_target + T_width) - K ]), 2) # Upper Bound UB
-                # T_range = np.round(np.arange((T_target - T_width) - K, (T_target + T_width) - K + T_step/2, T_step), 2) # [LB,UB] branch
+                T_range = np.round(np.arange((T_target - T_width), (T_target + T_width) + T_step/2, T_step), 2) # [LB,UB] branch
+                # T_range = np.round(np.array([-2.00]), 2) # fixed
 
-                base_index = 4
+                base_index = 3
                 K_increment = int(np.floor((K - 4.0) / 0.5 + 1e-10))
-                ell_increment = int(np.floor((ell1 - 1.0) / 0.5 + 1e-10))
-                choice_index = base_index + K_increment + ell_increment
-                # N_index = max(0, min(choice_index, len(N_choice) - 1))
-                # dt_index = max(0, min(choice_index, len(dt_choice) - 1))
-                # N = N_choice[N_index]
-                # dt = dt_choice[dt_index]
-                tempidx = 8
-                N = 640 #N_choice[tempidx]
-                dt = 5e-6 #dt_choice[tempidx]
-                MPIrank = 40 #max(1, N // 32) # 192 cores per node on Nibi
+                ell_increment = int(np.floor((ell1 - 1.0) / 2.5 + 1e-10))
+                choice_index = base_index + 2*K_increment + ell_increment
+                N_index = max(0, min(choice_index, len(N_choice) - 1))
+                dt_index = max(0, min(choice_index, len(dt_choice) - 1))
+                N = N_choice[N_index-0]
+                dt = dt_choice[dt_index]
+                MPIrank = rk_choice[N_index-0]
+                # tempidx = 8
+                # N = 384 #N_choice[tempidx]
+                # dt = 7e-9 #dt_choice[tempidx]
+                # MPIrank = 128 #max(1, N // 32) # 192 cores per node on Nibi
                 nodes = math.ceil(MPIrank / 192)
 
                 for T in T_range:
-                    mem_est = 1 * 8 * np.ceil((1.1 * np.exp( -4.789714989 + 0.83721882 * np.log10((N**2) * (10.0**T) / dt) - 1.70503490 * elltemp
-                                                + 0.23432402 * K + 0.18691756 * elltemp * np.log10((N**2) * (10.0**T) / dt) )) / 8.0 )
-                    mem_req = int(np.maximum( 4 , mem_est ))
+                    # mem_est = 0.9 * 8 * np.ceil((1.1 * np.exp( -4.789714989 + 0.83721882 * np.log10((N**2) * (10.0**T) / dt) - 1.70503490 * elltemp
+                    #                             + 0.23432402 * K + 0.18691756 * elltemp * np.log10((N**2) * (10.0**T) / dt) )) / 8.0 )
+                    mem_req = 4 * MPIrank #int(np.maximum( 4 , mem_est ))
                     mem = f"{mem_req}G" 
                     addmemcount = mem_req
-                    addfilecount = ( N**2 / 4e7 ) * 1e-3 * np.power(10.0,T) / dt
+                    addfilecount = ( N**2 / 5e7 ) * 1e-3 * np.power(10.0,T) / dt
                     memcount += addmemcount
                     filecount += addfilecount
                     tasks.append((float(K), float(ell1), float(ell2), float(T), float(dt), int(N), int(MPIrank), int(nodes), mem))
@@ -132,7 +133,7 @@ def write_runscripts(tasks, out_dir='runscripts'):
         fname = run_dir / f"run_{K}_{ell1:.2f}_{ell2:.2f}_{T:.2f}_{dt:.0e}_{N}_{mpi_ranks}r.sh"
         with fname.open('w') as fh:
             fh.write(f"""#!/bin/bash
-#SBATCH --account=rrg-bprotas
+#SBATCH --account=def-bprotas
 #SBATCH --mail-user=zigicj@mcmaster.ca
 #SBATCH --mail-type=ALL
 #SBATCH --nodes={nodes}
@@ -173,13 +174,13 @@ def write_run_array_sh(groups, tag, out_fname='run_array.sh', max_concurrent=0):
         if SEQUENTIAL_TASKS:
             ### sequential groups ###
             lines.append('if [[ -z "${prev_jobid:-}" ]]; then')
-            lines.append(f"    prev_jobid=$(sbatch --parsable --account=rrg-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \\")
+            lines.append(f"    prev_jobid=$(sbatch --parsable --account=def-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \\")
             lines.append(f"                     --job-name={RUN_ARRAY_NAME} --nodes={nodes} \\")
             lines.append(f"                     --ntasks={mpi_ranks} --cpus-per-task=1 --time={SBATCH_TIME} --mem-per-cpu={mem_per_cpu_mib}M \\")
             lines.append(f"                     --array={array_spec} --output=slurm_logs/slurm-%A_%a.out --error=slurm_logs/slurm-%A_%a.err \\")
             lines.append(f"                     --export=ALL,PARAM_FILE={param_file} ./run_task_array.sh)")
             lines.append("  else")
-            lines.append(f"    prev_jobid=$(sbatch --parsable --dependency=afterany:${{prev_jobid}} --account=rrg-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \\")
+            lines.append(f"    prev_jobid=$(sbatch --parsable --dependency=afterany:${{prev_jobid}} --account=def-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \\")
             lines.append(f"                     --job-name={RUN_ARRAY_NAME} --nodes={nodes} \\")
             lines.append(f"                     --ntasks={mpi_ranks} --cpus-per-task=1 --time={SBATCH_TIME} --mem-per-cpu={mem_per_cpu_mib}M \\")
             lines.append(f"                     --array={array_spec} --output=slurm_logs/slurm-%A_%a.out --error=slurm_logs/slurm-%A_%a.err \\")
@@ -187,7 +188,7 @@ def write_run_array_sh(groups, tag, out_fname='run_array.sh', max_concurrent=0):
             lines.append("  fi")
         else: 
             ### concurrent groups ###
-            lines.append(f"prev_jobid=$(sbatch --parsable --account=rrg-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \\")
+            lines.append(f"prev_jobid=$(sbatch --parsable --account=def-bprotas --mail-user=zigicj@mcmaster.ca --mail-type=ALL \\")
             lines.append(f"                 --job-name={RUN_ARRAY_NAME} --nodes={nodes} \\")
             lines.append(f"                 --ntasks={mpi_ranks} --cpus-per-task=1 --time={SBATCH_TIME} --mem-per-cpu={mem_per_cpu_mib}M \\")
             lines.append(f"                 --array={array_spec} --output=slurm_logs/slurm-%A_%a.out --error=slurm_logs/slurm-%A_%a.err \\")
@@ -205,13 +206,13 @@ def main():
     print(f"Parameter combinations (Max 500 concurrent jobs): {len(tasks)}")
     print(f"Filespace required: {(filecount)}K")
     print(f"Memory requested: {(memcount)}GB")
-    print("File count check: find . -type f -printf '%h\n' | sort | uniq -c | sort -nr")
-    print("Disk usage check: du -h | sort -hr")
+    print("File count check: find . -mindepth 2 -type f -printf '%h\n' | sed 's#^\(\./[^/]*\)/.*#\1#' | sort | uniq -c | sort -nr")
+    print("Disk usage check: du -h -d 1 | sort -hr")
     groups, tag = write_runscripts(tasks)
     write_run_array_sh(groups, tag, out_fname=RUN_ARRAY_NAME, max_concurrent=0)
     print(f"Experiment directory: runscripts/{tag}")
     print(f"Execution script: bash {RUN_ARRAY_NAME}")
-    print("Priority check: sshare -l -A def-bprotas_cpu,rrg-bprotas_cpu -u fabianbl,fu6,zigicj,noahb")
+    print("Priority check: sshare -l -A def-bprotas_cpu,rrg-bprotas_cpu -u fu6,zigicj,noahb")
 
 if __name__ == "__main__":
     main()
