9d3933692126230855c468a2cbb7b9d34c92631c
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2026-09-15 06:04:04 -0400

    Update postprocess.sh

--- postprocess.sh
+++ postprocess.sh
@@ -1,11 +1,19 @@
-#!/usr/bin/env bash
-#SBATCH --account=def-bprotas
-#SBATCH --time=02:00:00
+#!/bin/bash
+#SBATCH --account=mth260100
+#SBATCH --partition=shared
+#SBATCH --job-name=postprocess
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
-#SBATCH --output=output/postprocess_%j.out
+#SBATCH --mem=2G
+#SBATCH --time=00-01:59
+#SBATCH --output=slurm_logs/postprocess_%j.out
 #SBATCH --error=output/postprocess_%j.err
 
-module load StdEnv/2023 matlab
+module load python/3.9.5
+unset PYTHONPATH
+source "$PROJECT/x-jzigic/2DKS_Solver/.venv-postprocess/bin/activate"
+export MPLBACKEND=Agg
 
-bash ./Data/collectFigures.sh
+cd "$SCRATCH/Data"
+bash ./collectFigures.sh
